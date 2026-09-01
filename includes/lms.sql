ed3410ce4fac2f6480eef6754428c1c42159afee
Kaif <shahkaif327@gmail.com>
2026-09-01 12:28:55 +0500

    final save

--- includes/lms.sql
+++ includes/lms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2026 at 10:36 AM
+-- Generation Time: Sep 01, 2026 at 09:28 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
